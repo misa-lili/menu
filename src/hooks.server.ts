053c11f35d14861558dca9406e0cd32716459712
adt-hgkim <hgkim@gnmpm.com>
2023-05-03 16:57:48 +0900

    feat: CORS all

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,28 +2,21 @@ import type { Handle } from '@sveltejs/kit';
 
 export const handle: Handle = async ({ resolve, event }) => {
 	const response = await resolve(event);
-	const validDomains = /^(.*)?\.?example\.com$/;
-	let cors: string | false;
-
-	if (validDomains.test(event.url.hostname)) {
-		cors = `https://${event.url.hostname}`;
-	} else {
-		cors = false;
-	}
 
 	// Apply CORS header for API routes
 	if (event.url.pathname.startsWith('/api')) {
 		// Required for CORS to work
-		if (event.request.method === 'OPTIONS' && !!cors) {
+		if (event.request.method === 'OPTIONS') {
 			return new Response(null, {
 				headers: {
-					'Access-Control-Allow-Methods': 'POST, GET, OPTIONS, DELETE',
-					'Access-Control-Allow-Origin': cors
+					'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+					'Access-Control-Allow-Origin': '*',
+					'Access-Control-Allow-Headers': '*'
 				}
 			});
 		}
 
-		response.headers.append('Access-Control-Allow-Origin', cors.toString());
+		response.headers.append('Access-Control-Allow-Origin', `*`);
 	}
 
 	return response;
