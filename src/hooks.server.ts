eb7af36e0035154a867059443511060112850651
adt-hgkim <hgkim@gnmpm.com>
2023-05-03 16:53:10 +0900

    feat: cors

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,9 +1,30 @@
-// import type { Handle } from '@sveltejs/kit';
+import type { Handle } from '@sveltejs/kit';
 
-// export const handle = (async ({ event, resolve }) => {
-// 	// console.log(event);
+export const handle: Handle = async ({ resolve, event }) => {
+	const response = await resolve(event);
+	const validDomains = /^(.*)?\.?example\.com$/;
+	let cors: string | false;
 
-// 	const response = await resolve(event);
-// 	// console.log(response);
-// 	return response;
-// }) satisfies Handle;
+	if (validDomains.test(event.url.hostname)) {
+		cors = `https://${event.url.hostname}`;
+	} else {
+		cors = false;
+	}
+
+	// Apply CORS header for API routes
+	if (event.url.pathname.startsWith('/api')) {
+		// Required for CORS to work
+		if (event.request.method === 'OPTIONS' && !!cors) {
+			return new Response(null, {
+				headers: {
+					'Access-Control-Allow-Methods': 'POST, GET, OPTIONS, DELETE',
+					'Access-Control-Allow-Origin': cors
+				}
+			});
+		}
+
+		response.headers.append('Access-Control-Allow-Origin', cors.toString());
+	}
+
+	return response;
+};
